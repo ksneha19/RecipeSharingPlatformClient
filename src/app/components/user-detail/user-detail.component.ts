419d5071f4c4ba2be5d81de2b417d28179c67d04
ksneha19 <sneha.karingula.935@my.csun.edu>
2024-11-02 20:22:02 -0700

    committed 11/02/24

---
+++ src/app/components/user-detail/user-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
+
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+  user: User | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.userService.getUser(id).subscribe(user => {
+      this.user = user;
+    });
+  }
+}
